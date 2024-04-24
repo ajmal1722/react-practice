92609b693b44f57f2bbe1dd4be0c234f3e769152
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-04-24 16:10:10 +0530

    added location icon using react-icons

--- react-practice-traversy/src/assets/components/SingleJob.jsx
+++ react-practice-traversy/src/assets/components/SingleJob.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaMapMarker } from 'react-icons/fa'
 
 const SingleJob = ({ job }) => {
 
@@ -35,7 +36,7 @@ const SingleJob = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline mb-1 mr-1"/>
             {job.location}
           </div>
           <a
