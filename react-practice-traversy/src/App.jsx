193463a1eed1f4a0928b575a4fe480dd994ce925
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-04-24 19:01:46 +0530

    404 error page created

--- react-practice-traversy/src/App.jsx
+++ react-practice-traversy/src/App.jsx
@@ -3,12 +3,14 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import HomePage from './assets/pages/HomePage';
 import MainLayout from './assets/layoutes/MainLayout';
 import JobPage from './assets/pages/JobPage';
+import NotFound from './assets/pages/NotFound';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path={'/'} element={<MainLayout/>}>
       <Route index element={<HomePage/>} />
       <Route path={'/jobs'} element={<JobPage/>} />
+      <Route path={'*'} element={<NotFound/>} />
     </Route>
   )
 )
