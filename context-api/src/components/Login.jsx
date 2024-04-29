b95f9bb03d449d4fb9fc2323e3995044b594faca
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-04-29 12:25:28 +0530

    created new project context-api

---
+++ context-api/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+const Login = () => {
+
+    const [user, setUser] = 
+    return (
+        <div>
+            <h1>User Login</h1>
+        </div>
+    )
+}
+
+export default Login
