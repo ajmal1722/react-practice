b2f9c72cab8623ec26e106d2d9e62d92128873d0
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-15 19:42:05 +0530

    useEffect cleanUp

--- react-hooks/src/components/UseEffect.jsx
+++ react-hooks/src/components/UseEffect.jsx
@@ -1,39 +1,58 @@
 import { useState, useEffect } from 'react'
 
 const UseEffect = () => {
-    
+
     const [val, setVal] = useState(0)
-    const [color, setColor] = useState('white')
+    const [color, setColor] = useState('')
+    const [clearup, setClearup] = useState(false)
 
     useEffect(() => {
-        console.log('state changed', val)
-    })
+        setClearup(false)
+        const id = setTimeout(() => {
+            
+            setClearup(true)
+        }, val * 1000);
 
-    
+        console.log('state changed', val)
+
+        return () => {
+            clearTimeout(id)
+        }
+    }, [val])
+
+
 
     return (
-       <>
+        <>
+            
             <div>
                 <h3>useEffect</h3>
             </div>
-            <div className='border' style={{background: color}}>
+            { clearup && val && (
+                <div style={{background: 'gray', padding: '1rem', margin: '1rem'}}>
+                    <h1>Clear up function worked</h1>
+                    <h4>after {val} seconds </h4>
+                </div>
+            )
+            }
+            <div className='border' style={{ background: color }}>
                 <button onClick={() => setColor('purple')}>
                     Purple
                 </button>
                 <button onClick={() => setVal(val - 1)}>
-                    Decrement <span style={{fontSize: '20px', marginLeft: '5px'}}>-</span>
+                    Decrement <span style={{ fontSize: '20px', marginLeft: '5px' }}>-</span>
                 </button>
-                <span style={{fontSize: '20px', color: 'cyan'}}>
-                    { val }
+                <span style={{ fontSize: '20px', color: 'cyan' }}>
+                    {val}
                 </span>
                 <button onClick={() => setVal(val + 1)}>
-                    Increment <span style={{fontSize: '20px', marginLeft: '5px'}}>+</span>
+                    Increment <span style={{ fontSize: '20px', marginLeft: '5px' }}>+</span>
                 </button>
-                <button  onClick={() => setColor('orange')}>
+                <button onClick={() => setColor('orange')}>
                     Orange
                 </button>
             </div>
-       </> 
+        </>
     )
 }
 
