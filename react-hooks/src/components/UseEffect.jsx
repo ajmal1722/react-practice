3c05b5b6e4f40e028ce5a44c2d35c78117b2b1b2
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-15 18:50:37 +0530

    color changed in div

--- react-hooks/src/components/UseEffect.jsx
+++ react-hooks/src/components/UseEffect.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react'
 const UseEffect = () => {
     
     const [val, setVal] = useState(0)
-    const [color, setColor] = useState('')
+    const [color, setColor] = useState('white')
 
     useEffect(() => {
         console.log('state changed', val)
@@ -16,8 +16,8 @@ const UseEffect = () => {
             <div>
                 <h3>useEffect</h3>
             </div>
-            <div className='border'>
-                <button>
+            <div className='border' style={{background: color}}>
+                <button onClick={() => setColor('purple')}>
                     Purple
                 </button>
                 <button onClick={() => setVal(val - 1)}>
@@ -29,7 +29,7 @@ const UseEffect = () => {
                 <button onClick={() => setVal(val + 1)}>
                     Increment <span style={{fontSize: '20px', marginLeft: '5px'}}>+</span>
                 </button>
-                <button>
+                <button  onClick={() => setColor('orange')}>
                     Orange
                 </button>
             </div>
