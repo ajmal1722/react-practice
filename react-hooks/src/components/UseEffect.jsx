328db5a88b5a290e6e22b6ef81e091288920298b
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-05-15 18:17:48 +0530

    useEffect started

--- react-hooks/src/components/UseEffect.jsx
+++ react-hooks/src/components/UseEffect.jsx
@@ -2,14 +2,38 @@ import { useState, useEffect } from 'react'
 
 const UseEffect = () => {
     
-    
+    const [val, setVal] = useState(0)
+    const [color, setColor] = useState('')
+
+    useEffect(() => {
+        console.log('state changed', val)
+    })
 
     
 
     return (
-       <div>
-        
-       </div> 
+       <>
+            <div>
+                <h3>useEffect</h3>
+            </div>
+            <div className='border'>
+                <button>
+                    Purple
+                </button>
+                <button onClick={() => setVal(val - 1)}>
+                    Decrement <span style={{fontSize: '20px', marginLeft: '5px'}}>-</span>
+                </button>
+                <span style={{fontSize: '20px', color: 'cyan'}}>
+                    { val }
+                </span>
+                <button onClick={() => setVal(val + 1)}>
+                    Increment <span style={{fontSize: '20px', marginLeft: '5px'}}>+</span>
+                </button>
+                <button>
+                    Orange
+                </button>
+            </div>
+       </> 
     )
 }
 
