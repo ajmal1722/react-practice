76fa090ce3ab2ceec7a40cf60f0f259f5a515459
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-06-05 12:59:17 +0530

    todo completed

--- practice-todo/src/App.jsx
+++ practice-todo/src/App.jsx
@@ -1,33 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [inputVal, setInputVal] = useState('');
+  const [todo, setTodo] = useState(['sample']);
 
+  function handleSubmit (e) {
+    e.preventDefault();
+
+    setTodo([...todo, inputVal])
+
+    setInputVal('')
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h2>To-Do</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputVal}
+          onChange={(e) => setInputVal(e.target.value)}
+        />
+        <button type='submit'>
+           Submit
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      </form>
+      <div>
+        {
+          todo.map((item, index) => {
+            return <p key={index}>{item}</p>
+          })
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
